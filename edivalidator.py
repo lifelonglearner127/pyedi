cca161a994a4eba11cb892918c9b0ea9e03e0201
lifelonglearner127 <life.long.learner127@outlook.com>
2019-04-17 00:29:29 +0400

    remove whitespace nodes

--- edivalidator.py
+++ edivalidator.py
@@ -6,7 +6,20 @@ class EDIValidator:
     def __init__(self, file_name):
         self.file_name = file_name
         self.spec = xml.dom.minidom.parse(file_name)
+        self.remove_whitespace_nodes(self.spec, True)
 
+    def remove_whitespace_nodes(self,  node, unlink=False):
+        remove_list = []
+        for child in node.childNodes:
+            if child.nodeType == node.TEXT_NODE and \
+               not child.data.strip():
+                remove_list.append(child)
+            elif child.hasChildNodes():
+                self.remove_whitespace_nodes(child, unlink)
+        for node in remove_list:
+            node.parentNode.removeChild(node)
+            if unlink:
+                node.unlink()
 
 if __name__ == '__main__':
     pass
