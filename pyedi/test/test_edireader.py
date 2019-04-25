ab6ea7478cd1b6222a8fce0c84b232a08782e44a
lifelonglearner127 <life.long.learner127@outlook.com>
2019-04-25 21:29:38 +0400

    install pytest

---
+++ pyedi/test/test_edireader.py
@@ -0,0 +1,20 @@
+import pytest
+from pyedi.edireader import EDIReader
+class TestEDIReader:
+    def test_valid_856_edi(self):
+        pass
+
+    def test_invalid_start_end_tag(self):
+        pass
+
+    def test_invalid_segment(self):
+        pass
+
+    def test_invalid_element_length(self):
+        pass
+
+    def test_invalid_element_value(self):
+        pass
+
+    def test_invalid_element_type(self):
+        pass
